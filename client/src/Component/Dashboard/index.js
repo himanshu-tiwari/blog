8ff3b5cb45aa74600060ec721134e125aad7a6d5
himanshu-tiwari <htiwarih@gmail.com>
2019-02-03 18:01:14 +0530

    Added the basic setup for the client side

---
+++ client/src/Component/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const Dashboard = (props) => {
+    return(
+        <div className="dashboard">
+            Dashboard
+        </div>
+    );
+};
+
+export default Dashboard;
