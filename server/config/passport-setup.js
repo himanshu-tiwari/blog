7b57624385ac9c487a9df08e59ff038b4bc59075
himanshu-tiwari <htiwarih@gmail.com>
2019-02-03 19:20:27 +0530

    Added client keys

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,9 +1,11 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const { clientID, clientSecret } = require('./config').development.google;
 
 passport.use(
     new GoogleStrategy({
-        // config options
+        clientID,
+        clientSecret
     }), () => {
         // callback
     }
